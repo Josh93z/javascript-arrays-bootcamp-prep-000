45a76514d303c8b6d0e739d30fe705494df8a6dc
Joshua Zuza <joshua93z@hotmail.com>
2020-07-24 04:09:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-addElementToBeginningOfArray
+function destructivelyAddElementToBeginningOfArray(array,element){
+  
+  var alteredArray = array.unshift(element);
+  
+  return alteredArray;
+  
+}
